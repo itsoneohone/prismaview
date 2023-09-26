374c40f646f7b7309fbf276a6dd977c52d1dae70
oneohone <oneohone.space@proton.me>
2023-09-26 19:57:39 +0400

    update(db set up)
    - initial db set up via docker compose
    - set up prisma module to connect the app to the db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
-    console.log({ dto });
+  constructor(private prismaService: PrismaService) {}
+
+  async signup(dto: AuthDto) {
     // Get email and password
     // Check if the user exists
 
     // Hash the password
 
+    const userExists = await this.prismaService.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
     // Store the user in the DB
     return { msg: 'sign up fn' };
   }
