954b17f30f543afbeae474c7726623856b2e9a2e
oneohone <oneohone.space@proton.me>
2023-09-28 21:57:53 +0400

    add(session persistent storage with redis)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
+// import * as redisStore from 'cache-manager-redis-store';
+import { createClient } from 'redis';
+import RedisStore from 'connect-redis';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // redis connection logic
+  // const RedisStore = connectRedis(session);
+  const redisClient = createClient({
+    url: 'redis://localhost:6379',
+  });
 
   // Set up the session middleware
   app.use(
@@ -12,8 +21,16 @@ async function bootstrap() {
       secret: 'super_secret',
       resave: false,
       saveUninitialized: false,
+      store: new RedisStore({
+        client: redisClient,
+      }),
     }),
   );
+
+  // Connect to redis
+  await redisClient.connect().catch((error) => {
+    throw error;
+  });
 
   // Global param validator
   // - Transform param to the expected type as defined by TS
